00b88bb2b5be2bc7abbab534149d11d3b2d891b1
Soravit6488082 <89405328+Soravit6488082@users.noreply.github.com>
2024-02-10 18:21:58 +0700

    Add files via upload

---
+++ 6488082 Homework two page from project proposal/personal_info.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'weight_info.dart'; // Import WeightInputPage
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Information',
+      home: PersonalInfoPage(),
+    );
+  }
+}
+
+class PersonalInfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 15.0, top: 45.0),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => WeightInputPage()),
+                    );
+                  },
+                  child: Container(
+                    width: 90.0,
+                    height: 45.0,
+                    child: Image.asset('assets/buttonback.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Column(
+              children: [
+                Text(
+                  'Personal',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'Information',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildContainer('weight'),
+                _buildContainer('height'),
+              ],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => WeightInputPage()),
+              );
+            },
+            child: Container(
+              width: 345.0,
+              height: 52.0,
+              margin: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 20,
+                    offset: Offset(5, 10),
+                    spreadRadius: 0.1,
+                    blurStyle: BlurStyle.normal,
+                  ),
+                ],
+              ),
+              child: _buildNextButton(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNextButton() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF2ADF88), // Top color
+            Color(0xFF628F79), // Bottom color
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          'Next',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Jost',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContainer(String text) {
+    return Container(
+      width: 218.0,
+      height: 40.0,
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Color(0xFF2ADF88),
+            fontSize: 20.0,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'Jost',
+          ),
+        ),
+      ),
+    );
+  }
+}
